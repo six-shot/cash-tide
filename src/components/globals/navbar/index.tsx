d4e5b25a18b32910ca8f03163110023d52902012
six-shot <okhuomonajayi54@gmail.com>
2025-05-12 19:49:09 +0100

    first commit

--- src/components/globals/navbar/index.tsx
+++ src/components/globals/navbar/index.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   return (
     <div className="fixed top-5 left-0 right-0 z-50">
       <div className="w-full flex justify-center items-center">
-        <div className="w-[1100px] bg-white h-[72px] px-[38px] border border-[#EAECF0] rounded-[40px] flex justify-between items-center">
+        <div className="w-[1100px] nav-bg h-[72px] px-[38px] border border-[#EAECF0] rounded-[40px] flex justify-between items-center">
           <Image src={logo} alt="logo" width={107} height={33} />
           <nav className="flex items-center gap-[39px] text-sm font-medium font-[family-name:var(--font-inter)] text-[#13181F]">
             <Link href="/">Home</Link>
